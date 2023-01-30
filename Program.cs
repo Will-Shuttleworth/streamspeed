8fcea4b7d60b1386b1c8589d5787761ab204f975
Will Shuttleworth <willshuttleworth99@gmail.com>
2023-01-30 19:53:38 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace streamspeed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
